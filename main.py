89b0124740a83e0c09efe66bc1e0c71400ddca9f
kpom <kpomeroy@guard1services.com>
2015-05-20 16:44:22 -0500

    Be explicit with port?

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ def add():
     return render_template('add.html', team=team, api_key=api_key, description=description, organization=organization)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=int(os.environ.get('PORT', 5000)))

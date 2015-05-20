175def5c1f3fd93622b9d7566bb5d833dff34043
kpom <kpomeroy@guard1services.com>
2015-05-20 16:54:15 -0500

    Bind to everything

--- main.py
+++ main.py
@@ -41,4 +41,4 @@ def add():
     return render_template('add.html', team=team, api_key=api_key, description=description, organization=organization)
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
